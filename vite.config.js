cf620fd58656d20bb75274fa5e18c45c92d58698
KingMate05 <mateodayot05@gmail.com>
2025-10-06 03:04:24 -0400

    fix: vite build path for github actions

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from "vite";
-import { ViteMinifyPlugin } from "vite-plugin-minify";
-import { resolve } from "path";
-
 export default defineConfig({
   base: "/",
-  root: resolve(__dirname, "src"),
   plugins: [ViteMinifyPlugin({})],
   build: {
-    outDir: resolve(__dirname, "dist"),
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
