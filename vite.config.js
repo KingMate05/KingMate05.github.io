42a8a9f98b2f4776f058626a2fc5df385a7b6a55
KingMate05 <mateodayot05@gmail.com>
2025-10-06 02:28:11 -0400

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,15 @@ import { defineConfig } from "vite";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 export default defineConfig({
-  base: "/", // racine pour KingMate05.github.io
-  css: {
-    postcss: {},
-  },
-  sourcemap: true,
-  root: "src",
-
-  plugins: [
-    ViteMinifyPlugin({}), // juste le plugin minify
-  ],
-
+  base: "/", // racine pour un repo utilisateur
+  root: "src", // si ton index.html est dans src/
+  plugins: [ViteMinifyPlugin({})],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
-
     rollupOptions: {
       input: {
-        index: "./src/index.html",
+        main: "./src/index.html",
       },
     },
   },
