f3c4c79627fd1e3ac726b7928690e439971a50c0
KingMate05 <mateodayot05@gmail.com>
2025-10-06 02:55:35 -0400

    fix: remove rollupOptions to let vite handle index.html automatically

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,11 @@ import { defineConfig } from "vite";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 export default defineConfig({
-  base: "/", // car ton repo est kingmate05.github.io
+  base: "/", // ton site est à la racine (kingmate05.github.io)
   root: "src", // ton index.html est dans src/
   plugins: [ViteMinifyPlugin({})],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: "index.html", // <- Chemin relatif à "root"
-      },
-    },
   },
 });
