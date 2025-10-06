a0ec0968d2132ba0e89f2b428dec4d8eb8e9cd4c
KingMate05 <mateodayot05@gmail.com>
2025-10-06 02:36:22 -0400

    probleme de chemin

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 
 export default defineConfig({
-  base: "/", // racine pour un repo utilisateur
+  base: "/", // parfait car ton repo est kingmate05.github.io
   root: "src", // si ton index.html est dans src/
   plugins: [ViteMinifyPlugin({})],
   build: {
@@ -10,7 +10,7 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: "./src/index.html",
+        main: "index.html", // ✅ Corrigé
       },
     },
   },
